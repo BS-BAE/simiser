50dc717d90b24b137cc66b6bd0192bc77df250ca
baebs <baebs@mz.co.kr>
2018-01-12 10:59:50 +0900

    executor init

---
+++ instance-executor/simiser-intance-executor/src/main/java/com/simiser/executor/SimiserIntanceExecutorApplication.java
@@ -0,0 +1,12 @@
+package com.simiser.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiserIntanceExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiserIntanceExecutorApplication.class, args);
+	}
+}
